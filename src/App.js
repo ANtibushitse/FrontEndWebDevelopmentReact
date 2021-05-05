df7601600785ffeb84946303857239060e8c6774
Antibushitse <ntibushitse.arsene@gmail.com>
2021-05-05 18:28:55 +0200

    React Component part I

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {Navbar, NavbarBrand} from "reactstrap";
+import "./App.css";
+import Menu from "./components/MenuComponenets.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
       </div>
     );
   }
 }
-
 export default App;
